a0a6ee05eb0976f8b12085ece1a440dded31ebbc
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2015-12-08 14:06:19 +0100

    Fixed mv bug

--- provision.sh
+++ provision.sh
@@ -10,5 +10,7 @@ fi
 
 sed -i 's/ServerAlias\ 10.0.2.15/ServerAlias\ 10.0.3.11/' /etc/httpd/conf.d/15-horizon_vhost.conf
 rpm -q mod_auth_openidc || yum install -y https://github.com/pingidentity/mod_auth_openidc/releases/download/v1.8.6/mod_auth_openidc-1.8.6-1.el7.centos.x86_64.rpm
-mv /etc/httpd/conf.modules.d/10-auth_openidc.conf /etc/httpd/conf.d/auth_openidc.load || true
+if [ ! -f /etc/httpd/conf.d/auth_openidc.load ]; then
+  mv /etc/httpd/conf.modules.d/10-auth_openidc.conf /etc/httpd/conf.d/auth_openidc.load
+fi
 
