c25aa7bbd6bcde9f3f612adf8d1ad07f7d7db415
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2015-12-08 13:23:09 +0100

    Added auth mod openidc

--- provision.sh
+++ provision.sh
@@ -6,6 +6,8 @@ if [[ ! -f /var/tmp/provision_done ]]; then
   rpm -q openstack-packstack || yum install -y openstack-packstack
   packstack --answer-file=/opt/himlar/himlarconnect.txt \
    && touch /var/tmp/provision_done \
-   && reboot
+  rpm -q mod_auth_openidc || yum install -y https://github.com/pingidentity/mod_auth_openidc/releases/download/v1.8.6/mod_auth_openidc-1.8.6-1.el7.centos.x86_64.rpm
+  mv /etc/httpd/conf.modules.d/10-auth_openidc.conf /etc/httpd/conf.d/auth_openidc.load
+  reboot
 fi
 
