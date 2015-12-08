deaff416ec37e8d1aa32bb33566b52374282f436
Jan Ivar Beddari <code@beddari.net>
2015-12-08 12:34:02 +0100

    Fix looping provision script

--- provision.sh
+++ provision.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -eux
 
-if [[ ! -f /opt/provision_done ]]; then
+if [[ ! -f /var/tmp/provision_done ]]; then
   yum update -y
   rpm -q rdo-release || yum install -y https://www.rdoproject.org/repos/rdo-release.rpm
   rpm -q openstack-packstack || yum install -y openstack-packstack
   packstack --answer-file=/opt/himlar/himlarconnect.txt \
-   && touch /tmp/provision_done \
+   && touch /var/tmp/provision_done \
    && reboot
 fi
 
