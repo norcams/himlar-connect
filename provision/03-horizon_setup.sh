c027ce02678b79fc912df604fa741034dee83833
Jon K Hellan <jon.kare.hellan@uninett.no>
2015-12-10 11:47:49 +0100

    Fix typos

--- provision/03-horizon_setup.sh
+++ provision/03-horizon_setup.sh
@@ -2,7 +2,7 @@
 
 # Per http://adam.younglogic.com/2015/05/rdo-v3-only/
 # Add identity API v3 settings to Horizon config
-grep -q ^OPENSTACK_API_VERSIONS /etc/openstack/dashboard/local_settings || echo "
+grep -q ^OPENSTACK_API_VERSIONS /etc/openstack-dashboard/local_settings || echo "
 OPENSTACK_API_VERSIONS = {
      'identity': 3
 }
@@ -10,14 +10,14 @@ OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT = True
 OPENSTACK_KEYSTONE_DEFAULT_DOMAIN = 'Default'
 " >> /etc/openstack-dashboard/local_settings
 
-grep -q oidc /etc/openstack/dashboard/local_settings || echo "
+grep -q oidc /etc/openstack-dashboard/local_settings || echo "
 WEBSSO_ENABLED = True
 
 WEBSSO_CHOICES = (
       ("credentials", _("Keystone Credentials")),
       ("oidc", _("Dataporten")),
     )
-" >> /etc/openstack/dashboard/local_settings
+" >> /etc/openstack-dashboard/local_settings
 
 systemctl restart httpd
 
