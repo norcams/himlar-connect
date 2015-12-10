efbdcc2f50ee682f83517a5d4f67d753bad61488
Sigmund Augdal <sigmund.augdal@uninett.no>
2015-12-10 13:57:20 +0100

    Update idp, mapping and protocols if they exists

--- provision/08-create-keystone-stuff.py
+++ provision/08-create-keystone-stuff.py
@@ -13,13 +13,28 @@ headers = {
     'Content-Type': 'application/json',
 }
 with open('/opt/himlar/json/create-idp.json') as fh:
-    response = requests.put(baseurl + '/identity_providers/dataporten', headers=headers, data=fh.read())
+    data = fh.read()
+    response = requests.put(baseurl + '/identity_providers/dataporten',
+                            headers=headers, data=data)
+    if response.status_code == 409:
+        response = requests.patch(baseurl + '/identity_providers/dataporten',
+                                  headers=headers, data=data)
     response.raise_for_status()
 
 with open('/opt/himlar/json/create-mapping.json') as fh:
-    response = requests.put(baseurl + '/mappings/dataporten', headers=headers, data=fh.read())
+    data = fh.read()
+    response = requests.put(baseurl + '/mappings/dataporten',
+                            headers=headers, data=data)
+    if response.status_code == 409:
+        response = requests.patch(baseurl + '/mappings/dataporten',
+                                  headers=headers, data=data)
     response.raise_for_status()
 
 with open('/opt/himlar/json/create-protocol.json') as fh:
-    response = requests.put(baseurl + '/identity_providers/dataporten/protocols/oidc', headers=headers, data=fh.read())
+    data = fh.read()
+    response = requests.put(baseurl + '/identity_providers/dataporten/protocols/oidc',
+                            headers=headers, data=data)
+    if response.status_code == 409:
+        response = requests.patch(baseurl + '/identity_providers/dataporten/protocols/oidc',
+                                  headers=headers, data=data)
     response.raise_for_status()
