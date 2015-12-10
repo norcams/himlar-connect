f20fa4635531781979a8943beaa3728a91b43543
Jan Ivar Beddari <code@beddari.net>
2015-12-10 12:12:37 +0100

    FIX: No double quotes ...

--- provision/03-horizon_setup.sh
+++ provision/03-horizon_setup.sh
@@ -8,14 +8,12 @@ OPENSTACK_API_VERSIONS = {
 }
 OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT = True
 OPENSTACK_KEYSTONE_DEFAULT_DOMAIN = 'Default'
-" >> /etc/openstack-dashboard/local_settings
 
-grep -q oidc /etc/openstack-dashboard/local_settings || echo "
 WEBSSO_ENABLED = True
 
 WEBSSO_CHOICES = (
-      ("credentials", _("Keystone Credentials")),
-      ("oidc", _("Dataporten")),
+      ('credentials', _('Keystone Credentials')),
+      ('oidc', _('Dataporten')),
     )
 " >> /etc/openstack-dashboard/local_settings
 
